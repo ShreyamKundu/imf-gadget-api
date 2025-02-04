cce69c9c328105316de4c62ae568fa2bb3cc21da
Shreyam Kundu <shreyamkundu04@gmail.com>
2025-02-05 00:39:25 +0530

    404 page not found route added

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ app.use("/api/health-check", (req, res) => res.send("OK"));
 app.use("/api/auth", authRoutes);
 app.use("/api/gadgets", gadgetsRoutes);
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// 404 Route
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
